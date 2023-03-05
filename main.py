141b8facd33f02bf183617b0a65f2bf5cd0ef93c
Johannes Dittrich <johannes.dittrich@fau.de>
2023-03-06 00:15:28 +0100

    seasonic

--- main.py
+++ main.py
@@ -206,13 +206,14 @@ async def psu(message):
         await m.delete(delay=10)
         return
 
-    embed = discord.Embed(title='Tier A Netzteile (nach cultists.network)', color=discord.Color.blue(), url='https://cultists.network/140/psu-tier-list/')
+    embed = discord.Embed(title='Tier A Netzteile (nach cultists.network rev. 17.0a)', color=discord.Color.blue(), url='https://cultists.network/140/psu-tier-list/')
     embed.add_field(name='1000+W', value='https://geizhals.de/?cat=WL-2652571')
     embed.add_field(name='800+W', value='https://geizhals.de/?cat=WL-2652570')
     embed.add_field(name='700+W', value='https://geizhals.de/?cat=WL-2652569')
     embed.add_field(name='600+W', value='https://geizhals.de/?cat=WL-2652568')
     embed.add_field(name='500+W', value='https://geizhals.de/?cat=WL-2652566')
-    embed.add_field(name='Seasonic', value='https://geizhals.de/?cat=WL-2678896')
+    embed.add_field(name='Disclaimer:', value='Keine Garantie für Vollständigkeit und Aktualität!')
+    # embed.add_field(name='Seasonic', value='https://geizhals.de/?cat=WL-2678896')
     await message.reply(embed=embed)
 
 
