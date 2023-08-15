975c3a67df98b564edb1838dfe6cecc4105faf8c
jojodicus <jojodicus@gmail.com>
2023-08-15 21:01:30 +0200

    refactor

--- main.py
+++ main.py
@@ -37,8 +37,41 @@ bot = discord.Bot(intents=intents)
 minRole = cfg_commands["min_role"]
 
 async def send_msg_to_dev(msg):
-    jojo = await bot.fetch_user(226054688368361474) # Jojodicus#0001, bot dev
+    jojo = await bot.fetch_user(226054688368361474) # @jojodicus, bot dev
     await jojo.send(msg)
+
+function_name_map = {}
+function_alias_map = {}
+def register_command(name, aliases, function):
+    function_name_map[name] = function
+    function_alias_map[name] = aliases
+
+def is_recommendation_command(name):
+    return name in cfg_commands["recommendations"].keys()
+
+# TODO: switch to fuzzywuzzy, move to module
+def closest_match_key(phrase, options_dict):
+    # returns key of closest match
+    mindist = float('inf')
+    minkey = None
+
+    for key, values_list in options_dict.items():
+        if phrase in values_list:
+            return key
+
+        for value in values_list:
+            dist = distance(phrase, value, score_cutoff=2)
+            if dist < mindist and dist <= 2:
+                mindist = dist
+                minkey = key
+
+    return minkey
+
+
+async def error_reply(message, reply_text):
+    m = await message.reply(reply_text)
+    if config["ephemeral_errors"]:
+        await m.delete(delay=10)
 
 
 @bot.event
@@ -131,8 +164,7 @@ def matches_roughly(command, options):
 
 async def command_handler(message):
     if not has_role_or_higher(message.author, minRole, message.guild):
-        m = await message.reply(f'Du musst mindestens {minRole} sein, um Befehle zu nutzen')
-        await m.delete(delay=10)
+        await error_reply(f'Du musst mindestens {minRole} sein, um Befehle zu nutzen')
         return
 
     preproccessed = message.content.replace('(', ' ').replace(')', ' ')
@@ -140,43 +172,47 @@ async def command_handler(message):
 
     identifier = cmd[0].lower()
 
-    options = [
-        ['help', 'hilfe', 'command', 'befehl', 'commandlist'],
-        ['meta', 'metafrage'],
-        ['psu'],
-        ['ssd', '1tbssd', 'ssd1tb'],
-        ['2tbssd', 'ssd2tb'],
-        ['4tbssd', 'ssd4tb'],
-        ['aio', 'wasserkühlung', 'wasserkühler', 'allinone'],
-        ['case', 'gehäuse'],
-        ['cpukühler', 'cpu-kühler', 'cpu-cooler'],
-        ['lüfter', 'fan'],
-        ['netzteil', 'nt'],
-        ['ram'],
-        ['rgblüfter', 'rgb-fan'],
-        ['gpu-ranking', 'gpu-rank', 'gpu-benchmark', 'gpu'],
-        ['gidf', 'lmgtfy']
-    ]
-    options_func = [help, metafrage, psu, ssd_1tb, ssd_2tb, ssd_4tb, aio, case, cpukuehler, fans, netzteil, ram, rgbluefter, gpu_ranking, gidf]
+    best_match = closest_match_key(identifier, function_alias_map)
 
-    best_match = closest_match_index(identifier, options)
-
-    if best_match == -1:
+    if not best_match:
         return
 
-    await options_func[best_match](message, cmd)
+    if is_recommendation_command(best_match):
+        callback = recommendations_factory(best_match)
+    else:
+        callback = function_name_map[best_match]
+
+    await callback(message, cmd)
 
 
+# TODO: switch to returning embeds instead of reply-functions?
+def recommendations_factory(key):
+    cfg_cmd_reco = cfg_commands["recommendations"][key]
+    async def reply(message, cmd=None):
+        embed = discord.Embed(title=cfg_cmd_reco["title"], color=0x008380, url=cfg_cmd_reco["title_url"])
+        embed.set_thumbnail(url=cfg_cmd_reco["thumbnail_url"])
+        embed.add_field(name='', value=cfg_cmd_reco["message"])
+        await message.reply(embed=embed)
+    return reply
+
+# register recommendation commands
+# TODO: clean this up a little
+recommendation_commands = ["1tb_ssd", "2tb_ssd", "4tb_ssd", "aio", "case", "cpukühler", "lüfter", "netzteil", "ram", "rgblüfter"]
+for command in recommendation_commands:
+    register_command(command, cfg_commands["recommendations"][command]["aliases"], None)
+
+
+cfg_cmd_help = cfg_commands["help"]
 async def help(message, cmd=None):
-    embed = discord.Embed(title='Hilfe', color=discord.Color.blurple(), url='https://github.com/Jojodicus/bhw-dc-bot')
+    embed = discord.Embed(title=cfg_cmd_help["title"], color=discord.Color.blurple(), url=cfg_cmd_help["title_url"])
     embed.set_thumbnail(url=bot.user.display_avatar.url)
-    embed.add_field(name='', value='Eine Übersicht über alle Features und Befehle findest du auf der GitHub-Seite des Bots (Link im Titel).')
+    embed.add_field(name='', value=cfg_cmd_help["message"])
     await message.reply(embed=embed)
+register_command("help", cfg_cmd_help["aliases"], help)
 
 
+cfg_cmd_meta = cfg_commands["meta"]
 async def metafrage(message, cmd=None):
-    cfg_cmd_meta = cfg_commands["meta"]
-
     embed = discord.Embed(title=cfg_cmd_meta["title"], color=discord.Color.blurple(), url=cfg_cmd_meta["title_url"])
     embed.add_field(name='', value=cfg_cmd_meta["message"])
 
@@ -184,97 +220,20 @@ async def metafrage(message, cmd=None):
         await message.channel.send(embed=embed, reference=message.reference)
     else:
         await message.reply(embed=embed)
+register_command("meta", cfg_cmd_meta["aliases"], metafrage)
 
 
+cfg_cmd_psu = cfg_commands["psu"]
 async def psu(message, cmd=None):
-    cfg_cmd_psu = cfg_commands["psu"]
-
     embed = discord.Embed(title=cfg_cmd_psu["title"], color=discord.Color.brand_red(), url=cfg_cmd_psu["title_url"])
     for field in cfg_cmd_psu["fields"]:
         embed.add_field(name=field["title"], value=field["message"])
 
     await message.reply(embed=embed)
-
-# TODO: switch to returning embeds instead of reply-functions?
-def recommendations_factory(title, title_url, thumbnail_url, content):
-    async def reply(message, cmd=None):
-        embed = discord.Embed(title=title, color=0x008380, url=title_url)
-        embed.set_thumbnail(url=thumbnail_url)
-        embed.add_field(name='', value=content)
-        await message.reply(embed=embed)
-    return reply
-
-async def ssd_1tb(message, cmd=None):
-    embed = discord.Embed(title='1TB-SSDs', color=0x008380, url='https://gh.de/g/q0')
-    embed.set_thumbnail(url='https://images.samsung.com/is/image/samsung/p6pim/de/mz-v9p1t0bw/gallery/de-990pro-nvme-m2-ssd-mz-v9p1t0bw-533582557?$684_547_PNG$')
-    embed.add_field(name='', value='Für Bens Empfehlungen zu 1TB-SSDs klicke auf den Titel.')
-    await message.reply(embed=embed)
+register_command("psu", cfg_cmd_psu["aliases"], psu)
 
 
-async def ssd_2tb(message, cmd=None):
-    embed = discord.Embed(title='2TB-SSDs', color=0x008380, url='https://gh.de/g/qP')
-    embed.set_thumbnail(url='https://images.samsung.com/is/image/samsung/p6pim/de/mz-v9p1t0bw/gallery/de-990pro-nvme-m2-ssd-mz-v9p1t0bw-533582557?$684_547_PNG$')
-    embed.add_field(name='', value='Für Bens Empfehlungen zu 2TB-SSDs klicke auf den Titel.')
-    await message.reply(embed=embed)
-
-
-async def ssd_4tb(message, cmd=None):
-    embed = discord.Embed(title='4TB-SSDs', color=0x008380, url='https://gh.de/g/qW')
-    embed.set_thumbnail(url='https://images.samsung.com/is/image/samsung/p6pim/de/mz-v9p1t0bw/gallery/de-990pro-nvme-m2-ssd-mz-v9p1t0bw-533582557?$684_547_PNG$')
-    embed.add_field(name='', value='Für Bens Empfehlungen zu 4TB-SSDs klicke auf den Titel.')
-    await message.reply(embed=embed)
-
-
-async def aio(message, cmd=None):
-    embed = discord.Embed(title='AiO Wasserkühlungen', color=0x008380, url='https://gh.de/g/Xg')
-    embed.set_thumbnail(url='https://www.arctic.de/media/0b/7f/f3/1632824378/liquid-freezer-ii-280-argb-g00.png')
-    embed.add_field(name='', value='Für Bens Empfehlungen zu AiO Wasserkühlungen klicke auf den Titel.')
-    await message.reply(embed=embed)
-
-
-async def case(message, cmd=None):
-    embed = discord.Embed(title='Gehäuse', color=0x008380, url='https://gh.de/g/XY')
-    embed.set_thumbnail(url='https://endorfy.com/wp-content/products/EY2A006_Signum-300-ARGB/Media%20(pictures)/WebP/EY2A006-endorfy-signum-300-argb-01a-webp95.d20221216-u095934.webp')
-    embed.add_field(name='', value='Für Bens Empfehlungen zu Gehäusen klicke auf den Titel.')
-    await message.reply(embed=embed)
-
-
-async def cpukuehler(message, cmd=None):
-    embed = discord.Embed(title='CPU-Luftkühler', color=0x008380, url='https://gh.de/g/Xn')
-    embed.set_thumbnail(url='https://www.arctic.de/media/3c/68/58/1635319800/freezer_i35_argb_g00.png')
-    embed.add_field(name='', value='Für Bens Empfehlungen zu CPU-Luftkühlern klicke auf den Titel.')
-    await message.reply(embed=embed)
-
-
-async def fans(message, cmd=None):
-    embed = discord.Embed(title='Gehäuselüfter', color=0x008380, url='https://gh.de/g/q6')
-    embed.set_thumbnail(url='https://www.arctic.de/media/7b/fd/aa/1670325590/P12_MAX_G00.png')
-    embed.add_field(name='', value='Für Bens Empfehlungen zu Gehäuselüftern ohne RGB klicke auf den Titel.')
-    await message.reply(embed=embed)
-
-
-async def netzteil(message, cmd=None):
-    embed = discord.Embed(title='Netzteile', color=0x008380, url='https://gh.de/g/1H')
-    embed.set_thumbnail(url='https://www.corsair.com/medias/sys_master/images/images/h7b/hbc/9760776028190/base-rmx-2021-config/Gallery/RM850x_01/-base-rmx-2021-config-Gallery-RM850x-01.png_1200Wx1200H')
-    embed.add_field(name='', value='Für Bens Empfehlungen zu Netzteilen klicke auf den Titel.')
-    await message.reply(embed=embed)
-
-
-async def ram(message, cmd=None):
-    embed = discord.Embed(title='RAM', color=0x008380, url='https://gh.de/g/qC')
-    embed.set_thumbnail(url='https://www.gskill.com/_upload/images/156274365910.png')
-    embed.add_field(name='', value='Für Bens Empfehlungen zu RAM klicke auf den Titel.')
-    await message.reply(embed=embed)
-
-
-async def rgbluefter(message, cmd=None):
-    embed = discord.Embed(title='RGB-Gehäuselüfter', color=0x008380, url='https://gh.de/g/XQ')
-    embed.set_thumbnail(url='https://www.silentiumpc.com/wp-content/uploads/2021/03/spc235-spc-stella-hp-argb-120-pwm-rev11-01-png-www.png')
-    embed.add_field(name='', value='Für Bens Empfehlungen zu RGB-Gehäuselüftern klicke auf den Titel.')
-    await message.reply(embed=embed)
-
-
-# TODO: add rt
+# TODO: add rt, move to module
 async def find_image_gpu(resolution: str) -> str:
     async with aiohttp.ClientSession() as session:
         async with session.get('https://www.tomshardware.com/reviews/gpu-hierarchy,4388.html') as r:
@@ -302,29 +261,11 @@ async def find_image_gpu(resolution: str) -> str:
     await send_msg_to_dev(f'Could not find image for resolution {resolution}!')
     raise Exception('Could not find image')
 
-# TODO: switch to fuzzywuzzy
-# TODO: maybe use a dict
-def closest_match_index(phrase, options):
-    mindist = float('inf')
-    minindex = -1
 
-    for i, option in enumerate(options):
-        if phrase in option:
-            return i
-
-        for part in option:
-            dist = distance(phrase, part, score_cutoff=2)
-            if dist < mindist and dist <= 2:
-                mindist = dist
-                minindex = i
-
-    return minindex
-
-
+cfg_cmd_gpu = cfg_commands["gpu-ranking"]
 async def gpu_ranking(message, cmd):
     if len(cmd) < 2:
-        m = await message.reply(f'Bitte gib eine Auflösung an. Beispiel: `{prefix}gpu-ranking 1080p`')
-        await m.delete(delay=10)
+        await error_reply(f'Bitte gib eine Auflösung an. Beispiel: `{prefix}gpu-ranking 1080p`')
         return
 
     resolution = cmd[1:]
@@ -332,24 +273,13 @@ async def gpu_ranking(message, cmd):
     for res in resolution:
         res = res.lower()
 
-        fhd = ['1080', 'fhd', 'fullhd', '2k', '1920x1080']
-        wqhd = ['1440', 'qhd', 'wqhd', '2.5k', 'quadhd', '2560x1440']
-        uhd = ['2160', 'uhd', '4k', 'ultrahd', '3840x2160']
-        options = [fhd, wqhd, uhd]
-        idx = closest_match_index(res, options)
+        key = closest_match_key(res, cfg_cmd_gpu["resolutions"])
 
-        match idx:
-            case 0:
-                res = '1080p'
-            case 1:
-                res = '1440p'
-            case 2:
-                res = '2160p'
-            case _:
-                m = await message.reply(f'Unbekannte Auflösung: {res}')
-                await m.delete(delay=10)
-                return
-        cdn = await find_image_gpu(f'{res}-ult')
+        if not key:
+            await error_reply(f'Unbekannte Auflösung: {res}')
+            return
+
+        cdn = await find_image_gpu(f'{key}-ult')
 
         # save file if not already cached
         filename = cdn[cdn.rfind('/')+1:]
@@ -358,17 +288,20 @@ async def gpu_ranking(message, cmd):
             with open(filepath, 'wb') as f:
                 f.write(requests.get(cdn).content) # TODO: aiohttp
 
-        embed = discord.Embed(title=f'GPU-Ranking für {res}', url='https://www.tomshardware.com/reviews/gpu-hierarchy,4388.html', color=discord.Color.brand_red())
+        embed = discord.Embed(title=f'GPU-Ranking für {res}', url=cfg_cmd_gpu["title_url"], color=discord.Color.brand_red())
         file = discord.File(filepath, filename=filename)
         embed.set_image(url=f'attachment://{filename}')
         await message.reply(embed=embed, file=file)
+register_command("gpu-ranking", cfg_cmd_gpu["aliases"], gpu_ranking)
+
 
 # TODO: cpu ranking links thw
 
+
+cfg_cmd_gidf = cfg_commands["gidf"]
 async def gidf(message, cmd):
     if len(cmd) < 2:
-        m = await message.reply(f'Bitte gib einen Suchbegriff an. Beispiel: `{prefix}gidf wie funktioniert google`')
-        await m.delete(delay=10)
+        await error_reply(f'Bitte gib einen Suchbegriff an. Beispiel: `{prefix}gidf wie funktioniert google`')
         return
 
     searchterm = ' '.join(cmd[1:])
@@ -392,8 +325,8 @@ async def gidf(message, cmd):
 
     searchurl = f'https://www.google.com/search?q={searchterm}'.replace(' ', '+')
     embed = discord.Embed(title=f'GIDF: "{searchterm}"', url=searchurl, color=discord.Color.blurple())
-    embed.set_thumbnail(url='https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png')
-    embed.add_field(name='', value=f'Google ist dein Freund. Eine Suchmaschine zu benutzen ist kein Verbrechen. Hier eine Schnellübersicht der ersten paar Ergebnisse, die ganze Suche findest du im Link im Titel.')
+    embed.set_thumbnail(url=cfg_cmd_gidf["thumbnail_url"])
+    embed.add_field(name='', value=cfg_cmd_gidf["message"])
 
     txt = ''
     for i, e in enumerate(results):
@@ -401,9 +334,12 @@ async def gidf(message, cmd):
 
     embed.add_field(name='Suchergebnisse', value=txt)
     await message.reply(embed=embed)
+register_command("gidf", cfg_cmd_gidf["aliases"], gidf)
 
 
-@bot.slash_command(name='ping', description='Überprüft Vitalfunktionen des Bots')
+cfg_slashcommands = config["slash_commands"]
+
+@bot.slash_command(name='ping', description=cfg_slashcommands["ping"]["description"])
 @commands.has_permissions(administrator=True)
 async def ping(ctx):
     embed = discord.Embed(title=f'{bot.user.name} ist online', color=discord.Color.fuchsia())
@@ -416,7 +352,7 @@ async def ping(ctx):
     await ctx.respond(embed=embed, delete_after=30)
 
 
-@bot.slash_command(name='reload', description='Startet den Bot neu')
+@bot.slash_command(name='reload', description=cfg_slashcommands["reload"]["description"])
 @commands.has_permissions(administrator=True)
 async def reload(ctx):
     embed = discord.Embed(title=f'{bot.user.name} wird neu gestartet...', color=0x00ff00)
@@ -425,7 +361,7 @@ async def reload(ctx):
     os.execv(sys.executable, ['python3'] + sys.argv)
 
 
-@bot.slash_command(name='update', description='Aktualisiert und startet den Bot neu')
+@bot.slash_command(name='update', description=cfg_slashcommands["update"]["description"])
 @commands.has_permissions(administrator=True)
 async def update(ctx):
     embed = discord.Embed(title=f'{bot.user.name} wird aktualisiert...', color=0xffff00)
